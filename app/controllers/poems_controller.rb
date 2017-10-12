a08312ffc72084f8a6f8d298e7329ed82c271a03
鳥居健太郎 <kentaro@MacBook-Air.local>
2017-10-12 22:50:16 +0900

    dive14-2

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,24 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:show]
+
   def index
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    render json: @poem
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def poem_params
+      params.require(:poem).permit(:title, :content, :auther)
+    end
 end
